bd8a92f49b7ecd60ee05d5af0ed9f5c947a68e24
MaxSuslov <maxsusmail@gmail.com>
2022-07-27 23:16:59 +0200

    Events capturing - simple counter and mousemove event capturing with showing coordinates live

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 new Vue({
 	el: "#app", 
   data: {
-  	title: "Hello World!", 
-    link: "http://google.com",
-    finishedLink: '<a href="http://yahoo.com">Yahoo</a>'
+  	counter: 0,
+    x: 0,
+    y: 0
   },
   methods: {
-  	sayHello: function() {
-    	return this.title;
+  	increase: function() {
+    	this.counter++;
+    },
+    updateCoordinates: function(event) {
+    this.x = event.clientX;
+    this.y = event.clientY;
     }
   }
 });
