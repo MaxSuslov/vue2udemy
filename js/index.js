8f82a1ad8c5e05835ca88a344b8cff03080f961a
MaxSuslov <maxsusmail@gmail.com>
2022-07-29 10:47:15 +0200

    Custom attributes and passing the

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ new Vue({
     y: 0
   },
   methods: {
-  	increase: function() {
-    	this.counter++;
+  	increase: function(step, event) {
+    	this.counter += step;
     },
     updateCoordinates: function(event) {
     this.x = event.clientX;
