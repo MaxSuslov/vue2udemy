8646d4ce8ede14216bfebf682aa927db509f81be
MaxSuslov <maxsusmail@gmail.com>
2022-07-29 11:03:26 +0200

    Event modifiers simplified (without using additional function) - add modifiers through dot notation, multiple modifiers can be chained

--- js/index.js
+++ js/index.js
@@ -12,9 +12,6 @@ new Vue({
     updateCoordinates: function(event) {
     this.x = event.clientX;
     this.y = event.clientY;
-    }, 
-    dummy: function(event) {
-      event.stopPropagation();
     }
   }
 });
