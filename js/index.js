54a20544d54d4369cc0662ec209313eb99f94a6d
MaxSuslov <maxsusmail@gmail.com>
2022-07-29 12:21:49 +0200

    2-way (bi-directional) property  binding

--- js/index.js
+++ js/index.js
@@ -1,20 +1,6 @@
 new Vue({
 	el: "#app", 
   data: {
-  	counter: 0,
-    x: 0,
-    y: 0
-  },
-  methods: {
-  	increase: function(step, event) {
-    	this.counter += step;
-    },
-    updateCoordinates: function(event) {
-    this.x = event.clientX;
-    this.y = event.clientY;
-    },
-    alertMe: function() {
-      alert("You pressed the Enter or Space key!!");
-    }
+  	name: "Max"
   }
 });
