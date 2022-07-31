231c04c331541c0a0efa89171961a03ab5b2bc08
MaxSuslov <maxsusmail@gmail.com>
2022-07-31 17:52:13 +0200

    Computed - cached data concept

--- js/index.js
+++ js/index.js
@@ -4,8 +4,15 @@ new Vue({
   	counter: 0,
     secondCounter: 0
   }, 
+  computed: {
+    output: function() {
+      console.log("Computed");
+      return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+    }
+  },
   methods: {
     result() {
+      console.log("Method");
       return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
     }
   }
