64034bdbd827ee3dea88e8b959cdd37917bce6b2
MaxSuslov <maxsusmail@gmail.com>
2022-07-31 20:08:53 +0200

    DOM interactions - CSS classes

--- js/index.js
+++ js/index.js
@@ -1,27 +1,8 @@
 new Vue({
-	el: "#app", 
+	el: "#app",
   data: {
-  	counter: 0,
-    secondCounter: 0
-  }, 
-  computed: {
-    output: function() {
-      console.log("Computed");
-      return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
-    }
-  },
-  watch: {
-    counter: function(value) {
-      var vm = this;
-      setTimeout(function() {
-        vm.counter = 0;
-      }, 2000)
-    }
-  },
-  methods: {
-    result() {
-      console.log("Method");
-      return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
-    }
+    attachRed: false,
+    attachGreen: false,
+    attachBlue: false
   }
 });
