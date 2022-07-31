710e71ac6d5f1664015ac52c0473dcd3d4e8d2fc
MaxSuslov <maxsusmail@gmail.com>
2022-07-31 15:25:57 +0200

    Increase / Decrease bad coding style - unmaintainable

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 new Vue({
 	el: "#app", 
   data: {
-  	name: "Max"
+  	counter: 0,
+    result: ''
+  }, 
+  methods: {
+    increase: function() {
+      this.counter++;
+      this.result = this.counter < 5 ? 'Less than 5' : 'Greater than 5';
+    },
+    decrease: function() {
+      this.counter--;
+      this.result = this.counter < 5 ? 'Less than 5' : 'Greater than 5';
+    }
   }
 });
