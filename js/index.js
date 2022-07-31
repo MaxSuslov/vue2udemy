39ba211d73328a0228817df8e395dc1043165bca
MaxSuslov <maxsusmail@gmail.com>
2022-07-31 15:49:27 +0200

    Increase / Decrease - better solution but still not ideal

--- js/index.js
+++ js/index.js
@@ -2,16 +2,11 @@ new Vue({
 	el: "#app", 
   data: {
   	counter: 0,
-    result: ''
+    secondCounter: 0
   }, 
   methods: {
-    increase: function() {
-      this.counter++;
-      this.result = this.counter < 5 ? 'Less than 5' : 'Greater than 5';
-    },
-    decrease: function() {
-      this.counter--;
-      this.result = this.counter < 5 ? 'Less than 5' : 'Greater than 5';
+    result() {
+      return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
     }
   }
 });
