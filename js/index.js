5c53232376b173ca929da878d78979a8cb23955b
MaxSuslov <maxsusmail@gmail.com>
2022-08-02 22:34:30 +0200

    First exercise - basics

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
 new Vue({
-  el: "#app",
+  el: "#exercise",
   data: {
-    color: "grey",
-    width: 100,
+    name: "Max",
+    age: 44,
+    mainkun:
+      "https://i.pinimg.com/736x/38/c8/a7/38c8a7c492a91571dc1665527371c929.jpg",
   },
-  computed: {
-    myStyle: function () {
-      return {
-        // If you have dash or space in property name, use quotes or camelCase
-        "background-color": this.color,
-        // backgroundColor: this.color,
-        width: this.width + "px",
-      };
+  methods: {
+    randomFloat: function () {
+      let float = Math.random();
+      return float;
     },
   },
 });
+
+// ------ Alternative randomFloat solution customized -------
+// function getRandomFloat(min, max, decimals) {
+//   const str = (Math.random() * (max - min) + min).toFixed(decimals);
+
+//   return parseFloat(str);
+// }
+
+// console.log(getRandomFloat(1.5, 3.5, 2)); // 👉️ 1.96
+// console.log(getRandomFloat(1.5, 3.5, 3)); // 👉️ 2.036
+// console.log(getRandomFloat(1.5, 3.5, 4)); // 👉️ 3.0045
