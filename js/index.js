e74208fdae3c15cffa4094ad6efaa64e69e6875c
MaxSuslov <maxsusmail@gmail.com>
2022-08-02 17:57:17 +0200

    DOM CSS manipulation using computed styles

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 new Vue({
   el: "#app",
   data: {
-    attachRed: false,
-    color: "green",
+    color: "grey",
+    width: 100,
   },
   computed: {
-    divClasses: function () {
+    myStyle: function () {
       return {
-        red: this.attachRed,
-        blue: !this.attachRed,
+        // If you have dash or space in property name, use quotes or camelCase
+        "background-color": this.color,
+        // backgroundColor: this.color,
+        width: this.width + "px",
       };
     },
   },
