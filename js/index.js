39e5c88ec59666fd9143484e758d4fc246eb2fc8
MaxSuslov <maxsusmail@gmail.com>
2022-08-05 16:46:59 +0200

    v-for - looping through arrays + using <template> tag for output in non-nested tags

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 new Vue({
   el: "#app",
   data: {
-    show: true,
+    ingredients: ["meat", "fruit", "cookies"],
+    persons: [
+      { name: "Max", age: 27, color: "red" },
+      { name: "Anna", age: "unknown", color: "blue" },
+    ],
   },
 });
