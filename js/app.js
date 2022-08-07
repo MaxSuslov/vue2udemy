80ca50e1d1593dbb3a6387ea630c8d95930ba315
MaxSuslov <maxsusmail@gmail.com>
2022-08-07 21:52:40 +0200

    Accessing Vue instance properties in different ways

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
+let data = {
+  title: "The VueJS Instance",
+  showParagraph: false,
+};
+
 // vm is picked up here for "view model / Vue model", just use an arbitrary variable name
 let vm1 = new Vue({
   el: "#app1",
-  data: {
-    title: "The VueJS Instance",
-    showParagraph: false,
-  },
+  data: data,
   methods: {
     show: function () {
       this.showParagraph = true;
@@ -26,10 +28,17 @@ let vm1 = new Vue({
   },
 });
 
-setTimeout(function () {
-  vm1.title = "Changed by Timer!";
-}, 3000);
+// Accessing the Vue instance from outside. Commented out to get rid of annoying alerts caused by watch method
+// setTimeout(function () {
+//   vm1.title = "Changed by Timer!";
+//   vm1.show();
+// }, 3000);
 
+// This property is being added from outside, so it won't be passed to the constructor and cannot use the Vue internal methods, hence it's not reactive
+vm1.newProp = "Is not added to the constructor";
+console.log(vm1.$data === data);
+
+// Another Vue instance
 let vm2 = new Vue({
   el: "#app2",
   data: {
@@ -37,7 +46,10 @@ let vm2 = new Vue({
   },
   methods: {
     changeTitle: function () {
+      // Normal reference to a proxied property
       vm1.title = "Changed!";
+      // Another way of accessing instance properties
+      vm1.$data.title = "Changed!";
     },
   },
 });
