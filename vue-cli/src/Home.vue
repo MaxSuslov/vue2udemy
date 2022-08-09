9ad0b65ca01f565715908f8edd95c6d61385c10a
MaxSuslov <maxsusmail@gmail.com>
2022-08-09 13:10:54 +0200

    Creating a new component as a file, importing to another component locally, using it in its template (looping through 5), exporting as a global component and using it in the third component.

--- vue-cli/src/Home.vue
+++ vue-cli/src/Home.vue
@@ -1,22 +1,16 @@
 <template>
   <div>
-    <p>Server status: {{ status }}</p>
-    <hr />
-    <button @click="changeStatus">Change status</button>
+    <app-server-status v-for="server in 5"> </app-server-status>
   </div>
 </template>
 
 <script>
+// Importing a newly created component (ServerStatus is just an arbitrary name) to be able to use its template/data/methods
+import ServerStatus from "./ServerStatus.vue";
+// Registering it as a local component
 export default {
-  data: function () {
-    return {
-      status: "Critical",
-    };
-  },
-  methods: {
-    changeStatus() {
-      this.status = "Normal";
-    },
+  components: {
+    "app-server-status": ServerStatus,
   },
 };
 </script>
