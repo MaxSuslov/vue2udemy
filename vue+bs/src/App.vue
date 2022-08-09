600d65817de202000dc08d5e2ac496972fc14bff
MaxSuslov <maxsusmail@gmail.com>
2022-08-09 16:20:02 +0200

    Outsourced code to components files and structured them into folders

--- vue+bs/src/App.vue
+++ vue+bs/src/App.vue
@@ -1,37 +1,29 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <header>
-          <h1>Servers Status</h1>
-        </header>
-      </div>
-    </div>
+    <app-header></app-header>
     <hr />
     <div class="row">
-      <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-          <li class="list-group-item" v-for="index in 5">
-            Server #{{ index }}
-          </li>
-        </ul>
-      </div>
-      <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
-      </div>
+      <app-servers></app-servers>
+      <app-server-details></app-server-details>
     </div>
     <hr />
-    <div class="row">
-      <div class="col-xs-12">
-        <footer>
-          <p>All Servers are managed here</p>
-        </footer>
-      </div>
-    </div>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import Header from "./components/Shared/Header.vue";
+import Footer from "./components/Shared/Footer.vue";
+import Servers from "./components/Server/Servers.vue";
+import ServerDetails from "./components/Server/ServerDetails.vue";
+export default {
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-servers": Servers,
+    "app-server-details": ServerDetails,
+  },
+};
 </script>
 
 <style>
