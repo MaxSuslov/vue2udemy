94e088f5554a2dad9a77388080af1331ca189958
MaxSuslov <maxsusmail@gmail.com>
2022-08-10 10:08:35 +0200

    Added <style scoped> to the components - applied to these particular components. Without scoped attribute styles are applied globally

---
+++ vue_bs/src/components/Server/ServerDetails.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <p>Server Details are currently not updated</p>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid red;
+}
+</style>
