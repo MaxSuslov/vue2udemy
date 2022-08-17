4c44d982f60380ee4f9f6c9e965818537a4ab087
MaxSuslov <maxsusmail@gmail.com>
2022-08-17 20:49:12 +0200

    Setting a prop as an argument in the component tag in the  parent and using it in the child

--- quote/src/components/Quote.vue
+++ quote/src/components/Quote.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <p>'What a wonderful Quote!'</p>
+    <p>{{ quote }}</p>
   </div>
 </template> 
 
 <script>
+export default {
+  props: ["quote"],
+};
 </script>
 
 <style scoped>
