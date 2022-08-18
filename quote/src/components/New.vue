ed17c2edd601748dbeeb91a7330d27f3617363b5
MaxSuslov <maxsusmail@gmail.com>
2022-08-18 20:28:22 +0200

    Dynamic components are destroyed each time if going away and recreated from scratch if you return

--- quote/src/components/New.vue
+++ quote/src/components/New.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
     <h3>New</h3>
+    <button @click="counter++">Increase!</button>
+    <p>{{ counter }}</p>
   </div>
 </template> 
 
 <script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+};
 </script>
 
 <style scoped>
