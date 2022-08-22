a634a9c559a126f49547ab9108f68d1ccb6b23f9
MaxSuslov <maxsusmail@gmail.com>
2022-08-22 22:04:09 +0200

    Animated transitions between routes

--- routing/src/App.vue
+++ routing/src/App.vue
@@ -4,8 +4,12 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
         <hr />
+        <!-- Header will be displayed at the top on all pages except of /user (see named components in routes.js) -->
         <router-view name='header-top'></router-view>
-        <router-view></router-view>
+        <!-- Animation between different views/routes -->
+        <transition name='slide' mode='out-in'>
+            <router-view></router-view>
+        </transition>
         <router-view name='header-bottom'></router-view>
       </div>
     </div>
@@ -23,4 +27,37 @@ export default {
 </script>
 
 <style>
+    .slide-leave-active {
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
 </style>
+
