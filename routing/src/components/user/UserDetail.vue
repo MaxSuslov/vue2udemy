d9b0970733af2b4901f2caa908a8c784ff3cc914
MaxSuslov <maxsusmail@gmail.com>
2022-08-22 21:51:34 +0200

    Reserved slots for the named routes for displaying components in different places in html depending on route. Redirects and wildcards

--- routing/src/components/user/UserDetail.vue
+++ routing/src/components/user/UserDetail.vue
@@ -4,7 +4,7 @@
         <!-- We don't need any watcher here, because there is no way to load another component from here without rebuilding the component (we go back or to Home or to User anyway) -->
         <p>Loaded User ID is: {{ $route.params.id }}</p>
         <!-- we can pass an object here, e.g. a named route with its params, it's much more convenient-->
-        <router-link tag='button' :to='{ name: "userEdit", params: { id: $route.params.id }}' 
+        <router-link tag='button' :to='{ name: "userEdit", params: { id: $route.params.id }, query: {locale: "en", q: 100}}' 
             class = 'btn btn-primary'>Edit User</router-link>
     </div>
 </template>
