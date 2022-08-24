799b00c844e2cc0b85abff77201192e3d0f4c1e2
MaxSuslov <maxsusmail@gmail.com>
2022-08-24 19:57:59 +0200

    Form created with bi-directional binding and showing either the form if not submitted, or submitted data

--- Forms/src/App.vue
+++ Forms/src/App.vue
@@ -1,13 +1,30 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!submitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <h1>File a Complaint</h1>
                     <hr>
                     <div class="form-group">
+                        <label for="firstname">First Name</label>
+                        <input
+                                v-model="firstName"
+                                type="text"
+                                id="firstname"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname">Last Name</label>
+                        <input
+                                v-model="lastName"
+                                type="text"
+                                id="lastname"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
                         <label for="email">Mail</label>
                         <input
+                                v-model="email"
                                 type="text"
                                 id="email"
                                 class="form-control">
@@ -15,13 +32,23 @@
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input
+                                v-model="password"
                                 type="password"
                                 id="password"
                                 class="form-control">
                     </div>
                     <div class="form-group">
+                        <label>
+                            <input type="radio" value="Yes" v-model="storeData"> Yes
+                        </label>
+                        <label>
+                            <input type="radio" value="No" v-model="storeData"> No
+                        </label>
+                    </div>
+                    <div class="form-group">
                         <label for="age">Age</label>
                         <input
+                                v-model="age"
                                 type="number"
                                 id="age"
                                 class="form-control">
@@ -30,88 +57,26 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="message">Message</label><br>
-                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-                    <textarea
-                            id="message"
-                            rows="5"
-                            class="form-control"></textarea>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <div class="form-group">
-                        <label for="sendmail">
-                            <input
-                                    type="checkbox"
-                                    id="sendmail"
-                                    value="SendMail"> Send Mail
-                        </label>
-                        <label for="sendInfomail">
-                            <input
-                                    type="checkbox"
-                                    id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
-                        </label>
-                    </div>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="male">
-                        <input
-                                type="radio"
-                                id="male"
-                                value="Male"> Male
-                    </label>
-                    <label for="female">
-                        <input
-                                type="radio"
-                                id="female"
-                                value="Female"> Female
-                    </label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
-                    <label for="priority">Priority</label>
-                    <select
-                            id="priority"
-                            class="form-control">
-                        <option></option>
-                    </select>
-                </div>
-            </div>
-            <hr>
-            <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            type="submit" class="btn btn-primary" @click.prevent="submitted = true">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="submitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Mail:</p>
-                        <p>Password:</p>
-                        <p>Age:</p>
-                        <p>Message: </p>
-                        <p><strong>Send Mail?</strong></p>
-                        <ul>
-                            <li></li>
-                        </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
-                        <p>Switched:</p>
+                        <p>Full Name: {{ firstName }} {{ lastName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Age: {{ age }}</p>
+                        <p><strong>Store in Database? {{ storeData }}</strong></p>
                     </div>
                 </div>
             </div>
@@ -121,6 +86,17 @@
 
 <script>
     export default {
+        data() {
+            return {
+                email: '',
+                firstName: '',
+                lastName: '',
+                password: '',
+                age: '',
+                storeData: true,
+                submitted: false
+            }
+        }
     }
 </script>
 
