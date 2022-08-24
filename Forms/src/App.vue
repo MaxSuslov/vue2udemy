5e42ed82dc5c870906e6445bbe472c9ed5798d88
MaxSuslov <maxsusmail@gmail.com>
2022-08-24 20:43:35 +0200

    FullName is outsourced as a separate component, inputs are splitted and validated on input, altered instantly if changed

--- Forms/src/App.vue
+++ Forms/src/App.vue
@@ -3,24 +3,11 @@
         <form v-if="!submitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <h1>File a Complaint</h1>
+                    <h1>Submit your data</h1>
                     <hr>
-                    <div class="form-group">
-                        <label for="firstname">First Name</label>
-                        <input
-                                v-model="firstName"
-                                type="text"
-                                id="firstname"
-                                class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="lastname">Last Name</label>
-                        <input
-                                v-model="lastName"
-                                type="text"
-                                id="lastname"
-                                class="form-control">
-                    </div>
+
+                    <app-full-name v-model="fullName"></app-full-name>
+
                     <div class="form-group">
                         <label for="email">Mail</label>
                         <input
@@ -72,7 +59,7 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: {{ firstName }} {{ lastName }}</p>
+                        <p>Full Name: {{ fullName }}</p>
                         <p>Mail: {{ email }}</p>
                         <p>Password: {{ password }}</p>
                         <p>Age: {{ age }}</p>
@@ -85,17 +72,21 @@
 </template>
 
 <script>
+import FullName from './FullName.vue';
+
     export default {
         data() {
             return {
+                fullName: 'Max Suslov',
                 email: '',
-                firstName: '',
-                lastName: '',
                 password: '',
                 age: '',
-                storeData: true,
+                storeData: 'Yes',
                 submitted: false
             }
+        },
+        components: {
+            appFullName: FullName
         }
     }
 </script>
