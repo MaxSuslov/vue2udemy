a9d83e69716a0311a7557ef1c7b8171db6463523
MaxSuslov <maxsusmail@gmail.com>
2022-08-25 21:38:52 +0200

    Textarea - don't use {{interpolation}} between <textarea> - use v-model instead (for placeholder). To keep white-spaces and line-breaks use <textarea style='white-space: pre'

--- Forms/src/App.vue
+++ Forms/src/App.vue
@@ -11,18 +11,22 @@
                     <div class="form-group">
                         <label for="email">Mail</label>
                         <input
-                                v-model="email"
+                                v-model="userData.email"
                                 type="text"
                                 id="email"
                                 class="form-control">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
+                        <!-- .lazy is a modifier. Using it we update the input on.leave (not on every keystroke). 
+                        Another modifiers could be .trim (if you need to trim whitespeces) or .number if it should be converted to a number.
+ -->
                         <input
-                                v-model="password"
+                                v-model.lazy="userData.password"
                                 type="password"
                                 id="password"
                                 class="form-control">
+                                <p>{{ userData.password }}</p>
                     </div>
                     <div class="form-group">
                         <label>
@@ -35,12 +39,25 @@
                     <div class="form-group">
                         <label for="age">Age</label>
                         <input
-                                v-model="age"
+                                v-model="userData.age"
                                 type="number"
                                 id="age"
                                 class="form-control">
                     </div>
 
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea style="white-space: pre"
+                            id="message"
+                            rows="5"
+                            class="form-control"
+                            v-model="message"
+                            >
+                    </textarea>
                 </div>
             </div>
             <div class="row">
@@ -60,9 +77,10 @@
                     </div>
                     <div class="panel-body">
                         <p>Full Name: {{ fullName }}</p>
-                        <p>Mail: {{ email }}</p>
-                        <p>Password: {{ password }}</p>
-                        <p>Age: {{ age }}</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Age: {{ userData.age }}</p>
+                        <p>Message: {{ message }}</p>
                         <p><strong>Store in Database? {{ storeData }}</strong></p>
                     </div>
                 </div>
@@ -78,11 +96,14 @@ import FullName from './FullName.vue';
         data() {
             return {
                 fullName: 'Max Suslov',
+                userData: {
                 email: '',
                 password: '',
-                age: '',
+                age: 44,
+                },
                 storeData: 'Yes',
-                submitted: false
+                submitted: false,
+                message: 'Please write your message here'
             }
         },
         components: {
