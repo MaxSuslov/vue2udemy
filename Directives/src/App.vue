1e8cf947dd4e4ff9c4fbe6a8018c14f8268166f7
MaxSuslov <maxsusmail@gmail.com>
2022-08-26 19:15:44 +0200

    Custom modifier created - delayed as a value for setTimeout function

--- Directives/src/App.vue
+++ Directives/src/App.vue
@@ -13,7 +13,7 @@
                 <h1>Custom Directives</h1>
                 <!-- <p v-highlight="'red'">Color this</p> -->
                 <!-- :background will be treated as an argument in our directive. Here it is not have to be a string, we refer to it as to a string in main.js, it will be converted to a string behind the scene. If we remove :background, the text color will be red instead (v-if) -->
-                <p v-highlight:background="'red'">Color this</p>
+                <p v-highlight:background.delayed="'red'">Color this</p>
             </div>
         </div>
     </div>
--- Directives/src/main.js
+++ Directives/src/main.js
@@ -9,13 +9,18 @@ Vue.directive('highlight', {
 
     // binding is the information about our directive, and value is whatever we enter between the quotation marks v-highlight="'red'" to be able to set the value from outside
     // el.style.backgroundColor = binding.value;
-
-    // .arg is the information about our argument behind the : like :background
-    if (binding.arg == 'background') {
-      el.style.backgroundColor = binding.value;
-    } else {
-      el.style.color = binding.value;
+    let delay = 0;
+    // modifiers is an object, it can have multiple modifiers chained in App.vue
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
     }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 
