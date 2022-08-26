112adc1eaa34e76b56cc1832b9679393d5ed5d95
MaxSuslov <maxsusmail@gmail.com>
2022-08-26 17:17:09 +0200

    Setting value of binding from outside

--- Directives/src/main.js
+++ Directives/src/main.js
@@ -5,7 +5,10 @@ import App from './App.vue';
 // The second argument is an object with configuration (based on 5 hooks)
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
-    el.style.backgroundColor = 'green';
+    // el.style.backgroundColor = 'green';
+
+    // binding is the information about our directive, and value is whatever we enter between the quotation marks v-highlight="'red'" to be able to set the value from outside
+    el.style.backgroundColor = binding.value;
   }
 });
 
