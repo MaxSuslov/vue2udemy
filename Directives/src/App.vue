113bbf49aff19f10fe9a84f399b1c64f0f3923b3
MaxSuslov <maxsusmail@gmail.com>
2022-08-26 19:08:19 +0200

    Using binding.arg and conditional styling in custom directive

--- Directives/src/App.vue
+++ Directives/src/App.vue
@@ -11,7 +11,9 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
-                <p v-highlight>Color this</p>
+                <!-- <p v-highlight="'red'">Color this</p> -->
+                <!-- :background will be treated as an argument in our directive. Here it is not have to be a string, we refer to it as to a string in main.js, it will be converted to a string behind the scene. If we remove :background, the text color will be red instead (v-if) -->
+                <p v-highlight:background="'red'">Color this</p>
             </div>
         </div>
     </div>
--- Directives/src/main.js
+++ Directives/src/main.js
@@ -8,7 +8,14 @@ Vue.directive('highlight', {
     // el.style.backgroundColor = 'green';
 
     // binding is the information about our directive, and value is whatever we enter between the quotation marks v-highlight="'red'" to be able to set the value from outside
-    el.style.backgroundColor = binding.value;
+    // el.style.backgroundColor = binding.value;
+
+    // .arg is the information about our argument behind the : like :background
+    if (binding.arg == 'background') {
+      el.style.backgroundColor = binding.value;
+    } else {
+      el.style.color = binding.value;
+    }
   }
 });
 
