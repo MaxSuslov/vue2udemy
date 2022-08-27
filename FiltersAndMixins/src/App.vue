6a2f79cb425168371bc2ab98277446451217d617
MaxSuslov <maxsusmail@gmail.com>
2022-08-27 22:53:08 +0200

    Better filtering with computed property - on the fly calculations without rerendering DOM (filters are very limited)

--- FiltersAndMixins/src/App.vue
+++ FiltersAndMixins/src/App.vue
@@ -4,6 +4,11 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
                 <p>{{ text | toUppercase | to-lowercase }}</p>
+                <hr>
+                <input v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -14,11 +19,20 @@
         data() {
             return {
                 text: 'Hello there!',
+                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+                filterText: ''
             }
         }, 
         filters: {
             toUppercase(value) {
                 return value.toUpperCase();
+            }
+        }, 
+        computed: {
+            filteredFruits() {
+                return this.fruits.filter((element) => {
+                    return element.match(this.filterText);
+                })
             }
         }
     }
