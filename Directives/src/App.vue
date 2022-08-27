c7eee00f8ccd2338e3b6f8e6ae4829ab004a23e0
MaxSuslov <maxsusmail@gmail.com>
2022-08-27 15:22:03 +0200

    Locally registering a custom directive with advanced features like setInterval inside of setTimeout and switching/blinking the colors

--- Directives/src/App.vue
+++ Directives/src/App.vue
@@ -14,6 +14,7 @@
                 <!-- <p v-highlight="'red'">Color this</p> -->
                 <!-- :background will be treated as an argument in our directive. Here it is not have to be a string, we refer to it as to a string in main.js, it will be converted to a string behind the scene. If we remove :background, the text color will be red instead (v-if) -->
                 <p v-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-highlight:background.delayed.blink="'red'">Color this too</p>
             </div>
         </div>
     </div>
@@ -21,6 +22,36 @@
 
 <script>
     export default {
+        directives: {
+            // Can be also in camelCase, should be prepended by v-... while using
+            'local-highlight': {
+                bind(el, binding, vnode) {
+                    let delay = 0;
+                    if (binding.modifiers['delayed']) {
+                        delay = 3000;
+                    }
+                    if (binding.modifiers['blink']) {
+                        let mainColor = binding.value;
+                        let secondColor = 'blue';
+                        let currentColor = mainColor;
+                        setTimeout(() => {
+                        setInterval(() => {
+                            currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+                            binding.arg == 'background' ? el.style.backgroundColor = currentColor : el.style.color = currentColor;
+                        }, 1000);
+                        }, delay);
+                    } else {
+                        setTimeout(() => {
+                        if (binding.arg == 'background') {
+                            el.style.backgroundColor = binding.value;
+                        } else {
+                            el.style.color = binding.value;
+                        }
+                        }, delay);
+                    }
+                }
+            }
+        }
     }
 </script>
 
