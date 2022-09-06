3033f704b6398918c5edae0e7863f4df9d3dbe9d
MaxSuslov <maxsusmail@gmail.com>
2022-09-06 19:49:34 +0200

    Mixins - a .js file exporting a const object holding data that you don't want to duplicate but use in multiple places. Imported as an object and used as an array of all imported mixins

--- FiltersAndMixins/src/App.vue
+++ FiltersAndMixins/src/App.vue
@@ -9,18 +9,24 @@
                 <ul>
                     <li v-for="fruit in filteredFruits">{{ fruit }}</li>
                 </ul>
+                <hr>
+                <app-list>
+
+                </app-list>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import List from './List.vue';
+import { fruitMixin } from './fruitMixin';
+
     export default {
+        mixins: [fruitMixin],
         data() {
             return {
                 text: 'Hello there!',
-                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-                filterText: ''
             }
         }, 
         filters: {
@@ -28,12 +34,8 @@
                 return value.toUpperCase();
             }
         }, 
-        computed: {
-            filteredFruits() {
-                return this.fruits.filter((element) => {
-                    return element.match(this.filterText);
-                })
-            }
+        components: {
+            appList: List
         }
     }
 </script>
