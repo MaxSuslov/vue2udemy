071b1414b0fb8d91aba56f26c997ef6218527945
MaxSuslov <maxsusmail@gmail.com>
2022-09-06 22:16:58 +0200

    Filters and Mixins - exercise 2 done with computed properties

--- FiltersMixinsTask/src/App.vue
+++ FiltersMixinsTask/src/App.vue
@@ -5,16 +5,19 @@
                 <h1>Filters & Mixins</h1>
                 <!-- Exercise 1) -->
                 <!-- Build a local Filter which reverses the Text it is applied on -->
+                <h3>Local filter</h3>
                 <p>{{ firstText | reverse }}</p>
                 <!-- Exercise 2 -->
                 <!-- Build a global Filter which counts the length of a word and it appends it -->
                 <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+                <h3>Global filter</h3>
                 <p>{{ secondText | calculateLength }}</p>
-
+                <hr>
                 <!-- Exercise 3 -->
                 <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
-                <p>{{ firstText | reverse }}</p>
-                <p>{{ secondText | calculateLength }}</p>
+                <h3>Computed properties</h3>
+                <p>{{ reversedTextComputed }}</p>
+                <p>{{ calculateLengthComputed }}</p>
                 <!-- Exercise 4 -->
                 <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
             </div>
@@ -35,6 +38,14 @@
                 secondText: 'Second Text'
             }
         }, 
+        computed: {
+            reversedTextComputed() {
+                return this.firstText.split("").reverse().join("");
+            },
+            calculateLengthComputed() {
+                return this.secondText + ' (' + this.secondText.length + ')';
+            }
+        }
     }
 </script>
 
