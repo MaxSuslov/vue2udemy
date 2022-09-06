e61aaf5e36747fdcb44e244049a2953e0620c564
MaxSuslov <maxsusmail@gmail.com>
2022-09-06 22:09:55 +0200

    Filters and Mixins exercises 1 and 2 done

--- FiltersMixinsTask/src/App.vue
+++ FiltersMixinsTask/src/App.vue
@@ -5,14 +5,16 @@
                 <h1>Filters & Mixins</h1>
                 <!-- Exercise 1) -->
                 <!-- Build a local Filter which reverses the Text it is applied on -->
-
+                <p>{{ firstText | reverse }}</p>
                 <!-- Exercise 2 -->
                 <!-- Build a global Filter which counts the length of a word and it appends it -->
                 <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+                <p>{{ secondText | calculateLength }}</p>
 
                 <!-- Exercise 3 -->
                 <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
-
+                <p>{{ firstText | reverse }}</p>
+                <p>{{ secondText | calculateLength }}</p>
                 <!-- Exercise 4 -->
                 <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
             </div>
@@ -22,6 +24,17 @@
 
 <script>
     export default {
+        filters: {
+            reverse(value) {
+                return value.split("").reverse().join("");
+            }
+        }, 
+        data() {
+            return {
+                firstText: 'First Text',
+                secondText: 'Second Text'
+            }
+        }, 
     }
 </script>
 
--- FiltersMixinsTask/src/main.js
+++ FiltersMixinsTask/src/main.js
@@ -1,17 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.filter('to-lowercase', function(value) {
-    return value.toLowerCase();
+Vue.filter('to-lowercase', function (value) {
+  return value.toLowerCase();
+});
+
+Vue.filter('calculateLength', (value) => {
+  return value + ' (' + value.length + ')';
 });
 
 Vue.mixin({
-    created() {
-        console.log('Global Mixin - Created Hook');
-    }
+  created() {
+    console.log('Global Mixin - Created Hook');
+  }
 });
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: (h) => h(App)
+});
