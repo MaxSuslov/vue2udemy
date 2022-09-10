ca4ec3d794e0f7873ab0451298b332dc03e7552c
MaxSuslov <maxsusmail@gmail.com>
2022-09-10 23:43:09 +0200

    Interceptors

--- Http/src/main.js
+++ Http/src/main.js
@@ -8,6 +8,18 @@ Vue.use(VueResource);
 Vue.http.options.root =
   'https://vue-http-aebd5-default-rtdb.europe-west1.firebasedatabase.app/data.json';
 
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if ((request.method = 'POST')) {
+    request.method = 'PUT';
+  }
+  next((response) => {
+    response.json = () => {
+      return { messages: response.body };
+    };
+  });
+});
+
 new Vue({
   el: '#app',
   render: (h) => h(App)
