541ebb4dc0cf51d233c16ad101a86c2eb946baf5
MaxSuslov <maxsusmail@gmail.com>
2022-09-11 17:22:18 +0200

    mapGetters with ... (spread) operator after installing 'babel-stage-2' npm package (--save-dev) to be able to create our own computed properties additionally

--- Vuex/src/components/AnotherResult.vue
+++ Vuex/src/components/AnotherResult.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
-      <p>Counter is: {{ counter }}</p>
-      <p>Number of Clicks: {{ clicks }}</p>
+      <p>Counter is: {{ doubleCounter }}</p>
+      <p>Number of Clicks: {{ counterString }}</p>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
     export default {
         computed: {
-            counter() {
-                return this.$store.getters.doubleCounter;
-            },
-            clicks() {
-              return this.$store.getters.counterString;
-            }
-        }
+// without spread operator '...' it will not work (object directly inside of an object). The ... tells JS: please, pull out all the properties and methods in the object you have here and create separate key:value pairs for each of them.
+          ...mapGetters([
+          'doubleCounter',
+          'counterString'
+        ])
+      }
     }
 </script>
--- Vuex/src/main.js
+++ Vuex/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-import { store } from './store/store';
+import { store } from './state/store';
 
 new Vue({
   el: '#app',
