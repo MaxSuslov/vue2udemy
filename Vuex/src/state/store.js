0c0a54658dc77485b11958805ff43886289eb133
MaxSuslov <maxsusmail@gmail.com>
2022-09-13 23:00:15 +0200

    Splitting central state into modules for specialized things and getters, mutations and actions as separate files to keep store.js lean

--- Vuex/src/state/store.js
+++ Vuex/src/state/store.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import counter from './modules/counter';
 
+import * as actions from './actions';
+import * as mutations from './mutations';
+import * as getters from './getters';
+
 Vue.use(Vuex);
 
 // .store() is a method containing an object where we configure that store
@@ -9,25 +13,10 @@ export const store = new Vuex.Store({
   state: {
     value: 0
   },
-  // "getters" like "state" are reserved names, we should not change the naming here.
-  // Inside of getters we can name them as we wish, add as many getters as you want. doubleCounter here is a function, which should return something (we are going to access this value in the end)
-  getters: {
-    value: (state) => {
-      return state.value;
-    }
-  },
-  mutations: {
-    updateValue: (state, payload) => {
-      state.value = payload;
-    }
-  },
-  actions: {
-    updateValue({ commit }, payload) {
-      commit('updateValue', payload);
-    }
-  },
+  getters,
+  mutations,
+  actions,
   modules: {
-    // aka counter: counter
     counter
   }
 });
