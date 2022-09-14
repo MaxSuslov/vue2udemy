da089f3a4a38b3427cb084310b9d8fca0443c3a3
MaxSuslov <maxsusmail@gmail.com>
2022-09-14 22:11:10 +0200

    Headers set up with axios - axios.defaults.headers...

--- Axios/src/main.js
+++ Axios/src/main.js
@@ -8,6 +8,10 @@ import store from './store'
 // We don't write Vue.use(axios) here, because it's a third-party package, we can use it just like this
 // defaults object contains default global axios settings (applied to any request), which otherwise can be set up as additional argument (object) directly in the request (for a specific request only)
 axios.defaults.baseURL = 'https://axios-e474e-default-rtdb.europe-west1.firebasedatabase.app'
+// You can define your custom headers here, and axios will merge them with its default headers
+axios.defaults.headers.common['Authorization'] = 'someGenericToken'
+// If we have some specific needs, we can target e.g. the get() request and this header will be added to get requests
+axios.defaults.headers.get['Accepts'] = 'application/json'
 
 
 new Vue({
