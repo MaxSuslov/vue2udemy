13692998efa99cd75f2af3ba2af31db46afe06bb
MaxSuslov <maxsusmail@gmail.com>
2022-09-14 21:38:55 +0200

    Accessing and using the returned data

--- Axios/src/components/dashboard/dashboard.vue
+++ Axios/src/components/dashboard/dashboard.vue
@@ -2,6 +2,7 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your email address: {{ email }}</p>
   </div>
 </template>
 
@@ -9,9 +10,30 @@
 import axios from 'axios';
 
   export default {
+    data() {
+      return {
+        email: ''
+      }
+    },
     created() {
       axios.get('https://axios-e474e-default-rtdb.europe-west1.firebasedatabase.app/users.json')
-      .then(res => console.log(res))
+      .then(res => {
+        console.log(res)
+        // we extract data from the response
+        const data = res.data
+        const users = []
+        // we schould loop through keys
+        for (let key in data) {
+          // it would be the value for this cryptic key
+          // users.push(data[key])
+          const user = data[key]
+          // we write the cryptic id to a new property (id) to keep it and not to lose
+          user.id = key
+          users.push(user)
+        }
+        console.log(users)
+        this.email = users[0].email
+        })
       .catch(res => console.log(error))
     }
   }
