f0c4fba196344e631ef864a174791e883ed391ab
MaxSuslov <maxsusmail@gmail.com>
2022-09-14 21:20:52 +0200

    get() request with axios on created() lifecycle hook

--- Axios/src/components/dashboard/dashboard.vue
+++ Axios/src/components/dashboard/dashboard.vue
@@ -5,6 +5,19 @@
   </div>
 </template>
 
+<script>
+import axios from 'axios';
+
+  export default {
+    created() {
+      axios.get('https://axios-e474e-default-rtdb.europe-west1.firebasedatabase.app/users.json')
+      .then(res => console.log(res))
+      .catch(res => console.log(error))
+    }
+  }
+</script>
+
+
 <style scoped>
   h1, p {
     text-align: center;
