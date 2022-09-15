06bcb80036cf758895b6a30c97fdb810553fc793
MaxSuslov <maxsusmail@gmail.com>
2022-09-15 21:44:42 +0200

    Navbar from Bootstrap added

--- FinalProject/src/App.vue
+++ FinalProject/src/App.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="container">
-        <router-view></router-view>
+        <app-header></app-header>
+        <div class="row">
+            <div class="col-xs-12">
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue';
     export default {
-
+        components: {
+            appHeader: Header
+        }
     }
 </script>
 
