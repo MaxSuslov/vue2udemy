f0ac92bbb87dffa6487cfa4d85e39d0ecc1146fe
MaxSus <maxsusmail@gmail.com>
2022-09-21 22:01:08 +0200

    Buy button check - disabled if <=0 or if not an integer. Cleaned unused entities (not created yet) to get rid of errors in console

--- FinalProject/src/components/Header.vue
+++ FinalProject/src/components/Header.vue
@@ -10,13 +10,14 @@
                     <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
                     <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
                 </ul>
-                <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
+                <strong class="navbar-text navbar-right">Funds: </strong>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#" @click="endDay">End Day</a></li>
+                    <li><a href="#" 
+                    >End Day</a></li>
                     <li
                             class="dropdown"
                             :class="{open: isDropdownOpen}"
-                            @click="isDropdownOpen = !isDropdownOpen">
+                            >
                         <a
                                 href="#"
                                 class="dropdown-toggle"
@@ -25,8 +26,8 @@
                                 aria-haspopup="true"
                                 aria-expanded="false">Save & Load <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#" @click="saveData">Save Data</a></li>
-                            <li><a href="#" @click="loadData">Load Data</a></li>
+                            <li><a href="#" >Save Data</a></li>
+                            <li><a href="#" >Load Data</a></li>
                         </ul>
                     </li>
                 </ul>
--- FinalProject/src/components/stocks/Stock.vue
+++ FinalProject/src/components/stocks/Stock.vue
@@ -3,8 +3,8 @@
     <div class="panel panel-success">
       <div class="panel-heading">
         <h3 class='panel-title'>
-          NAME 
-          <small>(Price: PRICE)</small>
+          {{ stock.name }} 
+          <small>(Price: {{ stock.price }})</small>
         </h3>
       </div>
     </div>
@@ -14,13 +14,38 @@
           type="number"
           class="form-control"
           placeholder="Quantity"
+          v-model="quantity"
         >
       </div>
       <div class="pull-right">
         <button 
           class="btn btn-success"  
+          @click="buyStock"
+          :disabled="quantity <=0 || !Number.isInteger(quantity) "
         >Buy</button>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    }
+  }
+}
+</script>
